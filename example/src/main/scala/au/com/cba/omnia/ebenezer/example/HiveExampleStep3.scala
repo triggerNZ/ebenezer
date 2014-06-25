6603ed424120edb68cc164306ad8e5086aa13c11
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-06-25 14:35:02 +1000

    Added a third example that combined a scalding job with a hive query.

---
+++ example/src/main/scala/au/com/cba/omnia/ebenezer/example/HiveExampleStep3.scala
@@ -0,0 +1,37 @@
+package au.com.cba.omnia.ebenezer.example
+
+import com.twitter.scalding._, TDsl._
+import com.twitter.scalding.typed.IterablePipe
+
+import org.apache.hadoop.hive.conf.HiveConf
+
+import au.com.cba.omnia.ebenezer.scrooge.hive._
+
+class HiveExampleStep3(args: Args) extends CascadeJob(args) {
+  val conf = new HiveConf()
+  val db = args("db")
+  val srcTable = args("src-table")
+  val dstTable = args("dst-table")
+
+  val intermediate = HiveParquetScroogeSource[Customer](db, srcTable, conf)
+  val output       = PartitionHiveParquetScroogeSink[String, Customer](db, dstTable, List("id" -> "string"), conf)
+
+  val data = List(
+    Customer("CUSTOMER-A", "Fred", "Bedrock", 40),
+    Customer("CUSTOMER-2", "Wilma", "Bedrock", 40),
+    Customer("CUSTOMER-3", "Barney", "Bedrock", 39),
+    Customer("CUSTOMER-4", "BamBam", "Bedrock", 2)
+  )
+
+  val jobs = List(
+    new Job(args) {
+      IterablePipe(data, flowDef, mode)
+        .write(intermediate)
+    },
+    HiveJob(
+      args, "example",
+      s"INSERT OVERWRITE TABLE $db.$dstTable PARTITION (id) SELECT id,name,address,age FROM $db.$srcTable",
+      intermediate, output
+    )
+  )
+}
