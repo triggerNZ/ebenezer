cf93c30a490c01804207aed09b63ffc5a82d4305
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-07-02 15:27:40 +1000

    Split out tools into separate subproject.
    This fixes downstream hive reflection issues caused by having core
    produce an assembly and that being included by the hive part.
    
    Also removed the build.sbt from the subprojects and put the config into
    build.scala.

--- project/build.sbt
+++ project/build.sbt
@@ -12,16 +12,11 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-uniform.project("ebenezer", "au.com.cba.omnia.ebenezer")
-
-uniformThriftSettings
-
-uniformDependencySettings
-
-uniformAssemblySettings
-
-libraryDependencies ++=
-  depend.hadoop() ++ depend.scalding() ++ depend.scalaz() ++ depend.testing() ++ Seq(
-    "com.twitter"             %  "parquet-cascading"  % "1.4.1",
-    "au.com.cba.omnia"        %% "thermometer"        % "0.1.0-20140604034707-ce7a9d3" % "test"
-  )
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-unchecked",
+  "-Ywarn-all",
+  "-Xlint",
+  "-feature",
+  "-language:_"
+)
--- project/build.scala
+++ project/build.scala
@@ -19,22 +19,80 @@ import Keys._
 import au.com.cba.omnia.uniform.core.standard.StandardProjectPlugin._
 import au.com.cba.omnia.uniform.core.version.UniqueVersionPlugin._
 import au.com.cba.omnia.uniform.dependency.UniformDependencyPlugin._
+import au.com.cba.omnia.uniform.thrift.UniformThriftPlugin._
+import au.com.cba.omnia.uniform.assembly.UniformAssemblyPlugin._
 
 object build extends Build {
-  type Sett = Project.Setting[_]
-
-  lazy val core = project.in(file("core"))
-  lazy val hive = project.in(file("hive"))
-                    .dependsOn(core)
-  lazy val example = project.in(file("example")).dependsOn(hive)
-
+  lazy val standardSettings =
+    Defaults.defaultSettings ++ uniformDependencySettings
 
   lazy val all = Project(
     id = "all",
     base = file("."),
-    settings = Seq[Sett](
+    settings = standardSettings ++ Seq(
       publishArtifact := false
     ),
     aggregate = Seq(core, hive)
   )
+
+  lazy val core = Project(
+    id = "core",
+    base = file("core"),
+    settings = 
+      standardSettings
+        ++ uniform.project("ebenezer", "au.com.cba.omnia.ebenezer")
+        ++ uniformThriftSettings
+        ++ Seq(
+          libraryDependencies ++=
+            depend.hadoop() ++ depend.scalding() ++ depend.scalaz() ++ depend.testing() ++ Seq(
+              "com.twitter"             %  "parquet-cascading"  % "1.4.1",
+              "au.com.cba.omnia"        %% "thermometer"        % "0.1.0-20140604034707-ce7a9d3" % "test"
+            )
+        )
+  )
+
+  lazy val hive = Project(
+    id = "hive",
+    base = file("hive"),
+    settings =
+      standardSettings
+        ++ uniform.project("ebenezer-hive", "au.com.cba.omnia.ebenezer.hive")
+        ++ uniformThriftSettings
+        ++ Seq(
+          libraryDependencies ++=
+            depend.hadoop() ++ depend.scalding() ++ depend.testing() ++
+            depend.omnia("cascading-beehaus", "0.2.0-20140702020841-14237e5") ++
+            Seq(
+              "au.com.cba.omnia"  %% "thermometer" % "0.1.0-20140604034707-ce7a9d3" % "test"
+            )
+        )
+  ).dependsOn(core)
+
+  lazy val tools = Project(
+    id = "tools",
+    base = file("tools"),
+    settings =
+      standardSettings
+        ++ uniform.project("ebenezer-tools", "au.com.cba.omnia.ebenezer.cli")
+        ++ uniformThriftSettings
+        ++ uniformAssemblySettings
+        ++ Seq(
+          libraryDependencies ++=
+            depend.hadoop() ++ depend.scalding() ++ depend.scalaz() ++ depend.testing()
+        )
+  ).dependsOn(core)
+
+  lazy val example = Project(
+    id = "example",
+    base = file("example"),
+    settings =
+      standardSettings
+        ++ uniform.project("ebenezer-example", "au.com.cba.omnia.ebenezer.example")
+        ++ uniformThriftSettings
+        ++ uniformAssemblySettings
+        ++ Seq(
+          libraryDependencies ++=
+            depend.hadoop() ++ depend.scalding() ++ depend.testing()
+        )
+  ).dependsOn(hive)
 }
