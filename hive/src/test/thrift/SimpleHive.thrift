d605559aa7720084d2ad700a85b9cbe45e30ffdd
Stephan Hoermann <srhoermann@gmail.com>
2015-01-05 13:05:53 +1100

    Moves SimpleHive.thrift from main to test.

--- hive/src/test/thrift/SimpleHive.thrift
+++ hive/src/test/thrift/SimpleHive.thrift
@@ -12,7 +12,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-#@namespace scala au.com.cba.omnia.ebenezer.hive
+#@namespace scala au.com.cba.omnia.ebenezer.scrooge.hive
 
 struct SimpleHive {
   1: string  stringfield
